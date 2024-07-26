e9161a6bec339610a006a3da417091e5aa58881a
raghab007 <pokhrelraghab3@gmail.com>
2024-07-26 11:13:02 +0545

    Base class added for entities

--- src/main/java/com/springBoot/e_learning/Models/BaseEntity.java
+++ src/main/java/com/springBoot/e_learning/Models/BaseEntity.java
@@ -1,19 +1,18 @@
 package com.springBoot.e_learning.Models;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
+import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
 @Data
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @MappedSuperclass
 public class BaseEntity {
     @Id
